82fe46af9a911a50983e88e14b9602f4dec25bd1
Christian <contact@stremarcontrol.com>
2026-04-13 18:28:14 +0200

    auto: 2026-04-13 18:28:14

--- x.c
+++ x.c
@@ -3,12 +3,12 @@
 
 typedef int8_t i8; //8 bits signed
 typedef int8_t i6;
-typedef int8_t i32;
+typedef int8_t i32; //most likely
 typedef int8_t i64;
 typedef int8_t u8; //8 bits unsigned
-typedef int8_t u16;
-typedef int8_t u32;
-typedef int8_t u64;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
 typedef float 32;
 typedef double f64;
