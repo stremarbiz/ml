acd98583389b705474660cfe8896c0b9015db509
Christian <contact@stremarcontrol.com>
2026-04-13 18:27:06 +0200

    auto: 2026-04-13 18:27:06

---
+++ x.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h> // integer types, guaranteed sizes, as below
+
+typedef int8_t i8; //8 bits signed
+typedef int8_t i6;
+typedef int8_t i32;
+typedef int8_t i64;
+typedef int8_t u8; //8 bits unsigned
+typedef int8_t u16;
+typedef int8_t u32;
+typedef int8_t u64;
+
+typedef float 32;
+typedef double f64;
+
+int main()
+{
+    printf("Hello World\n");
+
+    return 0;
+}
