01b08a5a4eb0e962d60ae5911901ed7f8a4f23be
Christian <contact@stremarcontrol.com>
2026-04-13 18:32:07 +0200

    auto: 2026-04-13 18:32:07

--- x.c
+++ x.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdint.h> // integer types, guaranteed sizes, as below
 
-typedef int8_t i8; //8 bits signed
-typedef int8_t i6;
-typedef int8_t i32; //most likely
-typedef int8_t i64;
-typedef int8_t u8; //8 bits unsigned
+typedef int8_t i8; //8 bits signed (0 to positive)
+typedef int16_t i6;
+typedef int32_t i32; //most used  // negative 2147m to 2147m
+typedef int64_t i64;
+typedef uint8_t u8; //8 bits unsigned
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
-typedef float 32;
+typedef float f32; 
 typedef double f64;
 
 int main()
