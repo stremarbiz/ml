2204eebeb4aa9ff451356fca5e442bb76a1f7353
Christian <contact@stremarcontrol.com>
2026-04-13 13:38:22 +0200

    init

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "base.h"
+#include "arena.h"
+#include "prng.h"
+#include "arena.c"
+#include "prng.c"
+#include <stdio.h>
+
+typedef struct 
+{
+    u32 rows, cols;
+    f32* data;
+} matrix;
+
+matrix* mat_create(mem_arena* arena, u32 rows, u32 cols);
+
+int main(void)
+{
+    mem_arena* perm_arena = arena_create(GiB(1), MiB(1));
+
+    arena_destroy(perm_arena);
+
+    return 0;
+}
