2b841a1bbca277c6a2a1b435d85bb43e6705f150
Christian <contact@stremarcontrol.com>
2026-04-14 18:39:25 +0200

    auto: 2026-04-14 18:39:25

--- pointers.c
+++ pointers.c
@@ -26,7 +26,7 @@ int main()
 {
     i32 x = 123;
 
-    i32* px; //* can be at 2 or middle or before px; address corresponds to value of x (p)
+    i32* px = &x; //* can be at 2 or middle or before px; address corresponds to value of x (p)
     printf("Which value? Learn now");
 
     return 0;
